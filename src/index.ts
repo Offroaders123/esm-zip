a8b56b69d512d85c88a18a405dc654f2619fdce6
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-01-20 11:42:09 -0800

    Local File Header Distinction

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 export function readZip(data: Buffer){
+  return readLocalFileHeader(data);
+}
+
+export interface LocalFileHeader {
+  signature: string;
+  version: number;
+  generalBitFlag: number;
+  compressionMethod: number;
+  lastModifiedTime: number;
+  lastModifiedDate: number;
+  crc32: number;
+  compressedSize: number;
+  uncompressedSize: number;
+}
+
+export function readLocalFileHeader(data: Buffer): LocalFileHeader {
   const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
 
-  const signature = data.subarray(0,4).toString("utf-8");
-  const version = view.getUint16(4,true);
-  const generalBitFlag = view.getUint16(6,true);
-  const compressionMethod = view.getUint16(8,true);
-  const lastModifiedTime = view.getUint16(10,true);
-  const lastModifiedDate = view.getUint16(12,true);
-  const crc32 = view.getUint32(14,true);
-  const compressedSize = view.getUint32(18,true);
-  const uncompressedSize = view.getUint32(22,true);
+  const signature: string = data.subarray(0,4).toString("utf-8");
+  const version: number = view.getUint16(4,true);
+  const generalBitFlag: number = view.getUint16(6,true);
+  const compressionMethod: number = view.getUint16(8,true);
+  const lastModifiedTime: number = view.getUint16(10,true);
+  const lastModifiedDate: number = view.getUint16(12,true);
+  const crc32: number = view.getUint32(14,true);
+  const compressedSize: number = view.getUint32(18,true);
+  const uncompressedSize: number = view.getUint32(22,true);
 
   return { signature, version, generalBitFlag, compressionMethod, lastModifiedTime, lastModifiedDate, crc32, compressedSize, uncompressedSize };
 }
